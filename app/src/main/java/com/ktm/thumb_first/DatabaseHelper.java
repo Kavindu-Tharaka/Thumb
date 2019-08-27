4f1fd7dfba381e5638bd2ab39346288b89dcc5e4
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2019-08-27 18:26:08 +0530

    DatabaseHelper class is added

---
+++ app/src/main/java/com/ktm/thumb_first/DatabaseHelper.java
@@ -0,0 +1,45 @@
+package com.ktm.thumb_first;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "Thumb.db";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String SQL_CREATE_ENTRIES_SHOPPING_LIST = " CREATE TABLE " +ThumbMaster.ShoppingList.TABLE_NAME+
+                "( " +ThumbMaster.ShoppingList.COLUMN_NAME_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                ThumbMaster.ShoppingList.COLUMN_NAME_ITEM + "TEXT" +
+                ThumbMaster.ShoppingList.COLUMN_NAME_QUANTITY + "TEXT" +
+                ThumbMaster.ShoppingList.COLUMN_NAME_DATE + "TEXT" +
+                ThumbMaster.ShoppingList.COLUMN_NAME_ISBOUGHT + "INTEGER DEFAULT 0)";
+
+        db.execSQL(SQL_CREATE_ENTRIES_SHOPPING_LIST);
+
+/*        String SQL_CREATE_ENTRIES_DIARY = " CREATE TABLE " +ThumbMaster.ShoppingList.TABLE_NAME+
+                                        "( " +ThumbMaster.ShoppingList.COLUMN_NAME_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                        ThumbMaster.ShoppingList.COLUMN_NAME_ITEM + "TEXT" +
+                                        ThumbMaster.ShoppingList.COLUMN_NAME_QUANTITY + "TEXT" +
+                                        ThumbMaster.ShoppingList.COLUMN_NAME_DATE + "TEXT" +
+                                        ThumbMaster.ShoppingList.COLUMN_NAME_ISBOUGHT + "INTEGER DEFAULT 0)";
+
+        db.execSQL(SQL_CREATE_ENTRIES_DIARY);*/
+
+
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
