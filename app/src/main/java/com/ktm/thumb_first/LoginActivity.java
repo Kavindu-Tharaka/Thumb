022f37f934247828ccd3c2095fc1474aec5970c5
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2019-08-30 11:20:23 +0530

    Login part is added_2

--- app/src/main/java/com/ktm/thumb_first/LoginActivity.java
+++ app/src/main/java/com/ktm/thumb_first/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.ktm.thumb_first;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +15,7 @@ import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity {
 
 
-    public static boolean loginStatus = false;
+    public static boolean loginStatus = true;
 
     public static boolean getLoginStatus(){
         return loginStatus;
@@ -57,6 +59,23 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
 
+        builder.setMessage("Do you really want to exit?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        LoginActivity.super.onBackPressed();
+                    }
+                })
+                .setNegativeButton("No",null)
+        // .setCancelable(false)
+        ;
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 
 }
