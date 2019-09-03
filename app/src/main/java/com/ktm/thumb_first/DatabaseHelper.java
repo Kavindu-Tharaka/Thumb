750d7a2d3373c2d402393ca1b21f68b04dd8ba8d
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2019-09-04 01:20:01 +0530

    shopping list DB is almost finished

--- app/src/main/java/com/ktm/thumb_first/DatabaseHelper.java
+++ app/src/main/java/com/ktm/thumb_first/DatabaseHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "Thumb.db";
+    private static final String DATABASE_NAME = "ThumbDB.db";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
